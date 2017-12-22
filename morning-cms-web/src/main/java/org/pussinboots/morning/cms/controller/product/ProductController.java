54a0178dc62c5713aef4b0849a4797c1b00a31e1
zhancl <javazcl@aliyun.com>
2017-12-22 15:44:36 +0800

    商品的详情图片分类 控制器修改

--- morning-cms-web/src/main/java/org/pussinboots/morning/cms/controller/product/ProductController.java
+++ morning-cms-web/src/main/java/org/pussinboots/morning/cms/controller/product/ProductController.java
@@ -11,17 +11,13 @@ import org.pussinboots.morning.common.base.BaseController;
 import org.pussinboots.morning.common.base.BasePageDTO;
 import org.pussinboots.morning.common.constant.CommonReturnCode;
 import org.pussinboots.morning.common.support.page.PageInfo;
-import org.pussinboots.morning.product.entity.Category;
 import org.pussinboots.morning.product.entity.Product;
-import org.pussinboots.morning.product.entity.ProductDetail;
-import org.pussinboots.morning.product.entity.ProductImage;
 import org.pussinboots.morning.product.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  *
@@ -139,97 +135,7 @@ public class ProductController extends BaseController {
 		}
 	}
 //	=============================os_product_detail==============================================
-	/**
-	 * GET 更新商品详情信息
-	 * @return
-	 */
-	@ApiOperation(value = "更新商品详情信息", notes = "更新商品详情信息")
-	@RequiresPermissions("product:osDetail:update")
-	@GetMapping(value = "/{productId}/update")
-	public String getUpdateProductDetailPage(Model model, @PathVariable("productId") Long productId) {
-		// 广告信息
-		ProductDetail productDetail = productDetailService.selectByProductId(productId);
-		model.addAttribute("productDetail", productDetail);
-
-		return "/modules/product/productDetail_update";
-	}
-
-	/**
-	 * PUT 更新商品详情信息
-	 * @return
-	 */
-	@ApiOperation(value = "更新商品详情信息", notes = "根据productId修改")
-	@RequiresPermissions("product:osDetail:update")
-	@PutMapping(value = "/{productDetailId}")
-	@ResponseBody
-	public Object updateProductDetail(ProductDetail productDetail, @PathVariable("productDetailId") Long productDetailId) {
-
-		AuthorizingUser authorizingUser = SingletonLoginUtils.getUser();
-		if (authorizingUser != null) {
-			// 更新用户及广告记录
-			Integer count = productDetailService.updateProductDetail(productDetail, authorizingUser.getUserName(),productDetailId);
-			return new CmsResult(CommonReturnCode.SUCCESS, count);
-		} else {
-			return new CmsResult(CommonReturnCode.UNAUTHORIZED);
-		}
-	}
+ //ProductDetailController
 //	=============================os_product_image==============================================
-	/**
-	 * GET 商品图片页面
-	 * @return
-	 */
-	@ApiOperation(value = "商品图片页面", notes = "商品图片页面")
-	@RequiresPermissions("product:image:viewInfo")
-	@GetMapping(value = "/viewInfo")
-	public String getProductImagePage(Model model) {
-		return "/modules/product/productImage_list";
-	}
-
-	/**
-	 * GET 商品图片页面,
-	 * @return
-	 */
-	@ApiOperation(value = "获取商品图片页面列表", notes = "根据分页信息/搜索内容")
-	@RequiresPermissions("product:image:viewInfo")
-	@GetMapping(value = "/info")
-	@ResponseBody
-	public Object listProductImage(PageInfo pageInfo, @RequestParam(required = false, value = "search") String search, @PathVariable("productId") Long productId) {
-		BasePageDTO<ProductImage> basePageDTO = productImageService.listByPage(pageInfo, search,productId);
-		return new CmsPageResult(basePageDTO.getList(), basePageDTO.getPageInfo().getTotal());
-	}
-	/**
-	 * GET 修改商品图片详情信息
-	 * @return
-	 */
-	@ApiOperation(value = "修改商品图片详情信息", notes = "修改商品图片详情信息")
-	@RequiresPermissions("product:image:editInfo")
-	@GetMapping(value = "/{picImgId}/editInfo")
-	public String getUpdateProductImagelPage(Model model, @PathVariable("picImgId") Long picImgId) {
-
-		ProductImage productImage = productImageService.selectById(picImgId);
-		model.addAttribute("ProductImage", productImage);
-
-		return "/modules/product/productImg_update";
-	}
-
-	/**
-	 * PUT 修改商品图片详情信息
-	 * @return
-	 */
-	@ApiOperation(value = "修改商品图片详情信息", notes = "根据productId修改")
-	@RequiresPermissions("product:image:editInfo")
-	@PutMapping(value = "/{productImgId}")
-	@ResponseBody
-	public Object updateProductImage(ProductImage productImage, @PathVariable("picImgId") Long picImgId) {
-
-		AuthorizingUser authorizingUser = SingletonLoginUtils.getUser();
-		if (authorizingUser != null) {
-
-			Integer count = productImageService.updateProductImage(productImage, authorizingUser.getUserName(),picImgId);
-			return new CmsResult(CommonReturnCode.SUCCESS, count);
-		} else {
-			return new CmsResult(CommonReturnCode.UNAUTHORIZED);
-		}
-	}
-
+	//ProductImageController
 }
